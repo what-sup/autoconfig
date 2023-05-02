b6b4e6a7d9fa726797331a4cc60ac3ad136c9638
what-sup <54703293+what-sup@users.noreply.github.com>
2023-05-03 07:47:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,10 +23,10 @@ install()
 {
     if [[ x"${release}" == x"centos" ]]; then
         yum install epel-release -y
-        yum install wget curl unzip tar crontabs socat nano wget nload htop lsof sudo net-tools iptables -y
+        yum install wget curl unzip tar crontabs socat nano wget nload htop lsof sudo net-tools iptables screen software-properties-common gnupg2 -y
     else
         apt update -y
-        apt install wget curl unzip tar cron socat nano wget nload htop lsof sudo net-tools iptables -y
+        apt install wget curl unzip tar cron socat nano wget nload htop lsof sudo net-tools iptables screen software-properties-common gnupg2 -y
     fi
     wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh
     echo "$1"
