898f85ea5f4b3894dcc5c1b966aa81ef2043a8f5
what-sup <54703293+what-sup@users.noreply.github.com>
2023-05-02 09:27:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ install()
         yum install wget curl unzip tar crontabs socat nano wget nload htop lsof sudo -y
     else
         apt update -y
-        apt install wget curl unzip tar cron socat nano wget nload htop lsof sudo -y
+        apt install wget curl unzip tar cron socat nano wget nload htop lsof sudo net-tools -y
     fi
     wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh
     echo "$1"
@@ -34,6 +34,95 @@ install()
     wget https://${1}/${2}.zip -O ${2}.zip
     unzip -o -d /etc/XrayR $2.zip
     XrayR restart
+    read -e -r -p "iptables:speedtest? [Y/n] " input
+    case $input in
+        [yY][eE][sS] | [yY])
+            echo "Y"
+            speedtest=true
+        ;;
+        
+        [nN][oO] | [nN])
+            echo "N"
+        ;;
+        *)
+            echo "Y"
+            speedtest=true
+        ;;
+    esac
+    read -e -r -p "iptables:bt? [Y/n] " input
+    case $input in
+        [yY][eE][sS] | [yY])
+            echo "Y"
+            bt=true
+        ;;
+        
+        [nN][oO] | [nN])
+            echo "N"
+        ;;
+        *)
+            echo "Y"
+            bt=true
+        ;;
+    esac
+    read -e -r -p "iptables:mining? [Y/n] " input
+    case $input in
+        [yY][eE][sS] | [yY])
+            echo "Y"
+            mining=true
+        ;;
+        
+        [nN][oO] | [nN])
+            echo "N"
+        ;;
+        *)
+            echo "Y"
+            mining=true
+        ;;
+    esac
+    if [[ -z "${speedtest}" ]]; then
+        iptables -A OUTPUT -m string --string ".speed" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "speed." --algo bm -j DROP
+        iptables -A OUTPUT -m string --string ".speed." --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "fast.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "speedtest.net" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "speedtest.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "speedtest.cn" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "test.ustc.edu.cn" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "10000.gd.cn" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "db.laomoe.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "jiyou.cloud" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "ovo.speedtestcustom.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "speed.cloudflare.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "speedtest" --algo bm -j DROP
+    fi
+    if [[ -z "${bt}" ]]; then
+        iptables -A OUTPUT -m string --string "torrent" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string ".torrent" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "peer_id=" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "announce" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "info_hash" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "get_peers" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "find_node" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "BitTorrent" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "announce_peer" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "BitTorrent protocol" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "announce.php?passkey=" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "magnet:" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "xunlei" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "sandai" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "Thunder" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "XLLiveUD" --algo bm -j DROP
+    fi
+    if [[ -z "${mining}" ]]; then
+        iptables -A OUTPUT -m string --string "ethermine.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "antpool.one" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "antpool.com" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "pool.bar" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "get_peers" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "announce_peer" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "find_node" --algo bm -j DROP
+        iptables -A OUTPUT -m string --string "seed_hash" --algo bm -j DROP
+    fi
 }
 
 install $1 $2
